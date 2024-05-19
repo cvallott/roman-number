249fea57cdd4b6beff255ca14aba1e03658888cd
cvallott <caterina.vallotto@studenti.unipd.it>
2024-05-19 11:15:56 +0200

    close #11
    test ed implementazione conversione da 1 a 50

--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -11,14 +11,16 @@ import static org.junit.Assert.assertEquals;
 
 public class IntegerToRomanTest {
     @Test
-    public void testConvert1to20() {
+    public void testConvert1to50() {
         assertEquals("I", IntegerToRoman.convert(1));
         assertEquals("IV", IntegerToRoman.convert(4));
         assertEquals("V", IntegerToRoman.convert(5));
         assertEquals("IX", IntegerToRoman.convert(9));
         assertEquals("X", IntegerToRoman.convert(10));
         assertEquals("XX", IntegerToRoman.convert(20));
-        assertEquals("Numero non valido", IntegerToRoman.convert(21));
+        assertEquals("XL", IntegerToRoman.convert(40));
+        assertEquals("L", IntegerToRoman.convert(50));
+        assertEquals("Numero non valido", IntegerToRoman.convert(51));
         assertEquals("Numero non valido", IntegerToRoman.convert(0));
     }
 }
